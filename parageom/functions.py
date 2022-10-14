9fac33751d937842a7d03766b6785c7fc95212ad
JeoaFesketto <fesquet_jean@yahoo.fr>
2022-10-14 10:04:05 +0200

    added the show_section function 2D plots not yet implemented

--- parageom/functions.py
+++ parageom/functions.py
@@ -1,4 +1,8 @@
 import os
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
 
 from parablade.blade_match import BladeMatch
 from parablade.common.config import ReadUserInput, WriteBladeConfigFile
@@ -76,3 +80,53 @@ def make_geomTurbo(
         LE_fillet,
         TE_fillet,
     )
+
+
+def show_section(*geomTurbo_files, section = 0, _3Dimensional = False):
+
+    #TODO implement 2D plotting
+
+    t = time.time()
+
+    ax = plt.axes(projection="3d")
+    colors = [
+        'red',
+        'blue',
+        'yellow',
+        'orange',
+        'green',
+        'lime',
+        'pink',
+        'purple'
+    ]
+
+    for i, file in enumerate(geomTurbo_files):
+        rotor = Rotor(From_geomTurbo(file, 'sectioned'))
+
+        points = np.vstack(
+            (rotor.suction_sections[section], np.flip(rotor.pressure_sections[section], axis = 0))
+        )
+
+
+        ax.plot3D(points.T[0], points.T[2], points.T[1], colors[i])
+        print(f'\t\tplotted {file} in {colors[i]}')
+
+        if i == 0:
+            le = points[0]
+            te = rotor.suction_sections[section, -1]
+
+
+    # NOTE CHANGE THIS BACK
+    ax.set_xlim(le[0]-0.3, le[0]+0.3)
+    ax.set_zlim(le[1]-0.3, le[1]+0.3)
+    ax.set_ylim(le[2]-0.3, le[2]+0.3)
+    # ax.set_xlim(le[0]-le[1]/2, te[0]+le[1]/2)
+    # ax.set_zlim(le[1]-0.3, te[1]+0.3)
+    # ax.set_ylim(le[2]-0.3, te[2]+0.3)
+    ax.set_box_aspect(
+        [ub - lb for lb, ub in (getattr(ax, f"get_{a}lim")() for a in "xyz")]
+    )
+
+    print('This was generated in %(my_time).5f seconds\n' % {'my_time': time.time() - t})
+
+    plt.show()
