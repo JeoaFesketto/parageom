b39ffeccea7a00f53fd55a89dbb2bb9afaa23507
JeoaFesketto <fesquet_jean@yahoo.fr>
2022-10-26 17:19:11 +0200

    3d matching section per section untested

---
+++ bin/SectionalMatching.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+import argparse
+import json
+
+from parageom.common import print_parageom
+from parageom.case import Case
+
+
+print_parageom()
+
+# ARGUMENT DEFINITION
+
+parser = argparse.ArgumentParser(description="Make a .cfg file from a .geomTurbo file")
+parser.add_argument(
+    "geomTurbo_file",
+    help="Source .geomTurbo file to be matched.",
+    type=str,
+)
+parser.add_argument(
+    "Nsections",
+    help="Number of sections in output cfg file.",
+    default=10,
+    type=int,
+)
+parser.add_argument(
+    "-o",
+    "--output_folder",
+    help="Relative path to output folder.",
+    default="CASE",
+    type=str,
+)
+parser.add_argument(
+    "-d",
+    "--options_dict",
+    help=".json file with all the options for the case.",
+    default='config.json',
+    type=str,
+)
+parser.add_argument(
+    "-h",
+    "--on_hpc",
+    help="Flag to run non-interactively. Can also be defined inside of init.json.",
+    action="count",
+    default=0,
+)
+
+
+args = parser.parse_args()
+try:
+    options = json.load(args.options_dict)
+except:
+    options={}
+
+
+if bool(args.on_hpc):
+    options['on_hpc'] = True
+
+o = Case(
+    args.output_folder,
+    args.geomTurbo_file,
+    **options
+)
+o.match_blade(N_sections=args.Nsections)
