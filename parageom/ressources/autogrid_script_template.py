2c1d8cbb659ea57c61e7aa26167043e49865bf09
JeoaFesketto <fesquet_jean@yahoo.fr>
2022-11-16 17:00:39 +0100

    slightly improved outputs

--- parageom/ressources/autogrid_script_template.py
+++ parageom/ressources/autogrid_script_template.py
@@ -1,20 +1,19 @@
+output_dir = '_OUTPUT_DIR_'
+case_name =	'_CASE_NAME_'
+
 def log(msg):
-	f=open('script.log', 'a')
+	f=open(output_dir+'/'+'script.log', 'a')
 	print >> f, msg
 	f.close()
 
-f=open('script.log', 'w')
+f=open(output_dir+'/'+'script.log', 'w')
 print >> f, 'starting'
 f.close()
-
-output_dir = '_OUTPUT_DIR_'
-case_name =	'_CASE_NAME_'
 
 a5_open_project('_TEMPLATE_')
 log('opened project')
 
 a5_save_project(output_dir+'/'+case_name+'.trb')
-
 
 m = []
 for fn in dir(row(1).blade(1).get_hub_fillet()):
@@ -23,24 +22,24 @@ for fn in dir(row(1).blade(1).get_hub_fillet()):
 			m.append([fn, getattr(row(1).blade(1).get_hub_fillet(), fn)()])
 log(m)
 
-
-
 row(1).load_geometry('_GEOMTURBO_')
 log('replaced geometry')
 
-
 row(1).blade(1).get_hub_fillet().delete()
 row(1).add_hub_fillet()
-
 for elem in m:
-	log(elem)
 	getattr(row(1).blade(1).get_hub_fillet(), elem[0].replace('get', 'set'))(elem[1])
+log('new hub fillet created')
 
-
+log('saving project')
 a5_save_project(output_dir+'/'+case_name+'.trb')
+log('project saved')
 
+log('generating 3D mesh')
 a5_generate_3d()
+log('3D mesh generated')
 
+log('saving project')
 a5_save_project(output_dir+'/'+case_name+'.trb')
 log('done')
 
