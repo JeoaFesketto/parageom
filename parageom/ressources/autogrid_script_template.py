a29a82b9501c759e17a6f9a01b3cefbc78d56462
JeoaFesketto <fesquet_jean@yahoo.fr>
2022-12-01 16:47:28 +0100

    auto gaps and fillets

--- parageom/ressources/autogrid_script_template.py
+++ parageom/ressources/autogrid_script_template.py
@@ -67,6 +67,31 @@ def make_shroud_gap(row_i, params):
                         getattr(shroud_gap, elem[0].replace('get', 'set'))(elem[1])
         log('new shroud gap created')
 
+def get_values_hub_gap(row_i):
+        m = []
+        hub_gap = row(row_i).blade(1).get_hub_gap()
+        if hub_gap != 0:
+                for fn in dir(hub_gap):
+                        if callable(getattr(hub_gap, fn)):
+                                if fn.startswith('get') and fn != 'get_defined_shape':
+                                        m.append([fn, getattr(hub_gap, fn)()])
+                return m
+
+def make_hub_gap(row_i, params):
+        row(row_i).blade(1).get_hub_gap().delete()
+        row(row_i).add_hub_gap()
+        hub_gap = row(1).blade(1).get_hub_gap()
+        for elem in params:
+                if elem[0] == 'get_topology_type':
+                        if elem[1] == 0:
+                                hub_gap.set_topology_HO()
+                        elif elem[1] == 1:
+                                hub_gap.set_topology_O()
+                        elif elem[1] == 3:
+                                hub_gap.set_topology_O2H()
+                else:
+                        getattr(hub_gap, elem[0].replace('get', 'set'))(elem[1])
+        log('new hub gap created')
 
 f=open(output_dir+'/'+'script.log', 'w')
 print >> f, 'starting'
@@ -77,14 +102,31 @@ log('opened project')
 
 a5_save_project(output_dir+'/'+case_name+'.trb')
 
-m = get_values_hub_fillet(row_number)
-n = get_values_shroud_fillet(row_number)
+m = row(row_i).blade(1).get_hub_fillet()
+n = row(row_i).blade(1).get_shroud_fillet()
+g = row(row_i).blade(1).get_hub_gap()
+p = row(row_i).blade(1).get_shroud_gap()
+
+if m != 0:
+	m = get_values_hub_fillet(row_number)
+if n != 0:
+	n = get_values_shroud_fillet(row_number)
+if g != 0:
+	g = get_values_hub_gap(row_number)
+if p != 0:
+	p = get_values_shroud_gap(row_number)
 
 row(row_number).load_geometry('_GEOMTURBO_')
 log('replaced geometry')
 
-make_hub_fillet(row_number, m)
-make_shroud_fillet(row_number, n)
+if m != 0:
+	make_hub_fillet(row_number, m)
+if n != 0:
+	make_shroud_fillet(row_number, n)
+if g != 0:
+	make_hub_gap(row_number, g)
+if p != 0:
+	make_shroud_gap(row_number, p)
 
 log('saving project')
 a5_save_project(output_dir+'/'+case_name+'.trb')
